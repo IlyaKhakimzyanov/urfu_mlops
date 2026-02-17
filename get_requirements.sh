8306a723b18147a02cee9f31318af793cb8f26bd
Khakimzyanov.Ilya <161026khakimzyanov.ilya@users.no-reply.gitverse.ru>
2026-02-18 01:09:20 +0500

    just chill

---
+++ get_requirements.sh
@@ -0,0 +1,6 @@
+# не работает, пока просто руками буду
+
+#!/bin/bash
+# PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# pipreqs "$PROJECT_DIR" --force
+pipreqs . --force
---
+++ lab1/data_creation.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+data_folder = Path("source")
+print(data_folder)
