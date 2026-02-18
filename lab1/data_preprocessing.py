0568c1824c5a838a20caf3b54c5b042a4dc48f94
Khakimzyanov.Ilya <161026khakimzyanov.ilya@users.no-reply.gitverse.ru>
2026-02-19 00:29:00 +0500

    test

--- lab1/data_preprocessing.py
+++ lab1/data_preprocessing.py
@@ -9,7 +9,7 @@ train_folder_path = base_path / train_folder_name
 test_folder_path = base_path / test_folder_name
 
 #
-X_train = pd.read_csv(train_folder_path / 'X_train')
-y_train = pd.read_csv(train_folder_path / 'y_train')
-X_test = pd.read_csv(test_folder_path / 'X_test')
-y_test = pd.read_csv(test_folder_path / 'y_test')
+X_train = pd.read_csv(train_folder_path / 'X_train.csv')
+y_train = pd.read_csv(train_folder_path / 'y_train.csv')
+X_test = pd.read_csv(test_folder_path / 'X_test.csv')
+y_test = pd.read_csv(test_folder_path / 'y_test.csv')
