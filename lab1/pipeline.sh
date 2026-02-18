070d50a519f43b9dc73fda24a628b722235ca7e8
KhakimIlya <ilya.hakimzanov@gmail.com>
2026-02-18 23:55:05 +0500

    edit pipeline

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # надо отдельный наверное файл для проверки библиотек
+echo 'Проверка библиотек'
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$PROJECT_DIR"
 cd ..
 source start.sh > /dev/null 2>&1
+echo 'Ок!'
+echo 'Запуск pipline'
 
 #
 source .venv/bin/activate
+
+python lab1/data_creation.py
