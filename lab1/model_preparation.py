e8011e190c003e05b16c6e52a83e0c897ed93bc9
Khakimzyanov.Ilya <161026khakimzyanov.ilya@users.no-reply.gitverse.ru>
2026-02-22 22:12:05 +0500

    Change model

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -1,7 +1,8 @@
 import sys
 from pathlib import Path
 import pandas as pd
-from sklearn import svm
+# from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
 import pickle
 
 # Взятие рандома из .sh
@@ -21,12 +22,18 @@ X_train = pd.read_csv(train_folder_path / 'X_train.csv')
 y_train = pd.read_csv(train_folder_path / 'y_train.csv').iloc[:, 0]
 
 # Обучение
-clf1 = svm.SVC(kernel='linear', C=1, random_state=random_id)
+# clf1 = svm.SVC(kernel='linear', C=1, random_state=random_id)
+# clf1.fit(X_train, y_train)
+clf1 = DecisionTreeClassifier(
+    criterion='gini',
+    splitter='best',
+    random_state=random_id
+)
 clf1.fit(X_train, y_train)
 
 #
 model_folder_path.mkdir(exist_ok=True)
 
-pkl_filename = "SVC_model.pkl"
+pkl_filename = "clf_model.pkl"
 with open(model_folder_path / pkl_filename, 'wb') as file:
     pickle.dump(clf1, file)
